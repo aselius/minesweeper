20a905b38158997430784fcfe3fc512280976c58
Jaewoo Park <jaewoopark91@gmail.com>
2020-03-22 20:06:57 -0700

    Initialize all necessary function decs for render
    All the board function declarations were put in to remove compile
    errors.
    TODO: Fill in the business logic for rendering the full board.
    Fill in the logic for onclick and oncontext menu actions.
    Define and put in gameover conditions.

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,9 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class GameBoard extends React.Component {
-  state = {
-    board_data: this.getBoardData(),
+  gamestate = {
+    board_data: this.getBoardData(this.props.width, this.props.height, this.props.bombs),
     status: "Playing"
+  }
+
+  initialize_matrix(width, heght) {
+    return 1
+  }
+
+  initialize_random_bombs(data, bombs) {
+    return 1
+  }
+
+  calculate_cell_numbers(data) {
+    return 1
+  }
+
+  getBoardData(width, height, bombs) {
+    let data = this.initialize_matrix(width, height);
+    data = this.initialize_random_bombs(data, bombs);
+    data = this.calculate_cell_numbers(data);
+    return data;
+  }
+
+  render_game(data) {
+    return data
   }
 
   render() {
@@ -15,7 +38,7 @@ export default class GameBoard extends React.Component {
             {this.gamestate.status}
           </span>
         </div>
-        {this.renderGame(this.state.board_data)}
+        {this.render_game(this.gamestate.board_data)}
       </div>
     )
   };
